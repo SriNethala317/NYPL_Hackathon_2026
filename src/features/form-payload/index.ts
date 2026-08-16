25f1f7f6877dadc6c428e95530bb7238bdd8d2f3
barshatpradhan <barshatpradhan321@gmail.com>
2026-08-15 23:06:43 -0400

    feat: add form payload generation and mappings

---
+++ src/features/form-payload/index.ts
@@ -0,0 +1,3 @@
+export { generateFormPayload } from './generate-form-payload';
+export { PROGRAM_FORM_MAPPINGS } from './mappings';
+export type { FormFieldPrimitive, FormFieldValue, FormFillPayload, ProgramFormMapping } from './types';
